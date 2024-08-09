fe5e2c067bf34d49ecea1aad007ec7267b9f32f2
Izan128 <m.shimamura0225@gmail.com>
2024-08-09 19:09:00 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const btnPut = document.getElementById("btn");
+btnPut.addEventListener("click",() => {
+    const textChange = document.getElementById("text");
+    textChange.textContent="ボタンをクリックしました";
+});
